a3729a4349925e5b98164db0c2dd1dd5e2277db7
AaronBolarinho <aaron.bolarinho@gmail.com>
2018-10-01 18:23:47 +0000

    buffering stream assignemnt

--- http_requests/printHtml.js
+++ http_requests/printHtml.js
@@ -6,23 +6,34 @@ var https = require('https');
 // particular resource (like '/login/index.html'), that would be defined in the path
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step1.html'
+  path: '/http-examples/step2.html'
 };
-function getAndPrintHTMLChunks (response) {
 
+function getAndPrintHTMLChunks (response) {
 // notice that https.get takes a callback with one parameter -
 // response, which is a Stream that represents the HTTP response
 https.get(requestOptions, function (response) {
+  var finalString = "";
 
   // set encoding of received data to UTF-8
   response.setEncoding('utf8');
 
   // the callback is invoked when a `data` chunk is received
   response.on('data', function(chunk) {
+   // console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    finalString += chunk.toString();
+    console.log(finalString);
+  });
+})
+
+/* This is the original.
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function(chunk) {
     console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
     console.log(chunk.toString());
   });
 })
+*/
 
   // the callback is invoked when all of the data has been received
   // (the `end` of the stream)
@@ -33,24 +44,3 @@ https.get(requestOptions, function (response) {
 };
 
 https.request(requestOptions, getAndPrintHTMLChunks).end();
-
-
-/* This is version 1; works.
-var https = require('https');
-
-  var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
-  };
-
-
-function getAndPrintHTMLChunks (response) {
-
-  response.on('data', function(chunk) {
-    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
-    console.log(chunk.toString());
-  });
-}
-
-https.request(requestOptions, getAndPrintHTMLChunks).end();
-*/
