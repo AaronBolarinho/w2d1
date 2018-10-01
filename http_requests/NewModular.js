ecdbda70c239253d39f2e8a2e8415c02befbd381
AaronBolarinho <aaron.bolarinho@gmail.com>
2018-10-01 20:52:24 +0000

    3rd and 4th part of asignment

---
+++ http_requests/NewModular.js
@@ -0,0 +1,23 @@
+https.request(url,{},[callback]);
+
+
+var https = require('https');
+
+var requestOptions = {
+  host: 'example.com',
+  path: '/'
+};
+
+https.get(requestOptions, function (response) {
+
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data.length);
+  });
+
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+});
--- http_requests/printHtml.js
+++ http_requests/printHtml.js
@@ -1,46 +1,37 @@
-// while https is built-in to Node, it is a module, so it must be required
+
 var https = require('https');
 
-// the host can be thought of as the domain name you want to read from,
-// and the path is the resource - '/' is the root path, but if you wanted to read a
-// particular resource (like '/login/index.html'), that would be defined in the path
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step2.html'
-};
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+
 
 function getAndPrintHTMLChunks (response) {
-// notice that https.get takes a callback with one parameter -
-// response, which is a Stream that represents the HTTP response
-https.get(requestOptions, function (response) {
-  var finalString = "";
 
-  // set encoding of received data to UTF-8
-  response.setEncoding('utf8');
-
-  // the callback is invoked when a `data` chunk is received
   response.on('data', function(chunk) {
-   // console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
-    finalString += chunk.toString();
+    finalString = ""
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    finalString += chunk.toString()
     console.log(finalString);
   });
-})
-
-/* This is the original.
-  // the callback is invoked when a `data` chunk is received
-  response.on('data', function(chunk) {
-    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
-    console.log(chunk.toString());
-  });
-})
-*/
-
-  // the callback is invoked when all of the data has been received
-  // (the `end` of the stream)
-  response.on('end', function() {
-    console.log('Response stream complete.');
-  });
-
-};
+}
 
 https.request(requestOptions, getAndPrintHTMLChunks).end();
+
+// _____________________________________________________________________
+
+/*
+function getAndPrintHTML (options, response) {
+
+    response.on('data', function(chunk) {
+    finalString = ""
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    finalString += chunk.toString()
+    console.log(finalString);
+  });
+}
+
+https.request(getAndPrintHTMLChunks(requestOptions)).end();
+*/
+
